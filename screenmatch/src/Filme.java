afa0e9e50008f7dc207f3e8aeb2bbe1db79c8de5
Raphael Martins <laraqipe@gmail.com>
2025-03-20 20:01:03 -0300

    Aprendendo sobre classe, atributos e instanciação de objetos

---
+++ screenmatch/src/Filme.java
@@ -0,0 +1,13 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+
+
+
+
+}
